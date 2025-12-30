010321a93d86d3323f4e15f4c59af93b93666ae8
ertyhue <48786355+ertyhue@users.noreply.github.com>
2025-12-30 13:02:38 +0800

    Add files via upload

---
+++ Scripts/newbulid/communal.js
@@ -0,0 +1,14 @@
+﻿$(function () {
+	/*絲取����吓���������*/
+	$(".main-nav .menu .menu-hd").hover(function () {
+		$(this).parents(".menu").addClass("hover").end().siblings(".menu-bd").show();
+	}, function () {
+		$(this).parents(".menu").removeClass("hover").end().siblings(".menu-bd").hide();
+	});
+	$(".main-nav .menu .menu-bd").hover(function () {
+		$(this).show().parents(".menu").addClass("hover").end().siblings(".menu-hd").show();
+	}, function () {
+		$(this).hide().parents(".menu").removeClass("hover");
+	});
+	/*END絲取����吓���������*/
+})
